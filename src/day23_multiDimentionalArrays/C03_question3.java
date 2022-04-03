5d4198dd29140f7820b464c6f145fe42025f193f
archer1903 <98432117+archer1903@users.noreply.github.com>
2022-04-03 16:54:03 +0300

    Arrays Q3 added

--- src/day23_multiDimentionalArrays/C03_question3.java
+++ src/day23_multiDimentionalArrays/C03_question3.java
@@ -3,25 +3,22 @@ package day23_multiDimentionalArrays;
 public class C03_question3 {
     public static void main(String[] args) {
 
+        // sumSameIndex Seperately
+
         int arr1[][]={{1,2},{3,4,5},{6}};
         int arr2[][]={{7,8,9},{10,11},{12}};
 
         sumSameIndex(arr1,arr2);
-
     }
 
     public static void sumSameIndex(int[][] arr1, int[][] arr2) {
 
         String result="";
-
-
             for (int i = 0; i < arr1[i].length; i++) {
                 for (int j = 0; j < arr2[j].length ; j++) {
                     result+="["+i+"]"+"["+j+"] "+(arr1[i][j]+arr2[i][j])+"\n";
                 }
             }
-
-
         System.out.println(result);
     }
 }
