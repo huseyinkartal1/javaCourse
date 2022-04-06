e11e7b41d6147f0ed845635cd81fd0cafbfc8bf6
archer1903 <98432117+archer1903@users.noreply.github.com>
2022-04-06 16:06:20 +0300

    Arrays Q3 added

---
+++ src/day35_inheritance/Child01.java
@@ -0,0 +1,13 @@
+package day35_inheritance;
+
+public class Child01 extends Parent {
+    public static void main(String[] args) {
+
+        System.out.println(name);
+        System.out.println(company);
+
+        method1();
+        method2();
+
+    }
+}
