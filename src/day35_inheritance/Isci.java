2ada08f45cc15a79a9fe98d0f3915c39946a2cda
archer1903 <98432117+archer1903@users.noreply.github.com>
2022-04-06 16:10:49 +0300

    Isci Class added

---
+++ src/day35_inheritance/Isci.java
@@ -0,0 +1,21 @@
+package day35_inheritance;
+
+public class Isci extends Personel {
+    public static void main(String[] args) {
+
+        Personel objPers=new Personel();
+        objPers.isim="Eren";
+        objPers.soyisim="Terzi";
+        objPers.adres="Amsterdam";
+
+        Isci objIsci = new Isci();
+        objIsci.isim="Huseyin";
+        objIsci.soyisim="Efe";
+        objIsci.adres="Ankara";
+
+
+
+
+
+    }
+}
