39051891d8b6deee8111224617d1254cabc61b49
archer1903 <98432117+archer1903@users.noreply.github.com>
2022-04-13 17:51:58 +0300

    C01

---
+++ src/day40_overriding_polymorphism/C01.java
@@ -0,0 +1,11 @@
+package day40_overriding_polymorphism;
+
+public class C01 {
+
+    public void method1(){
+        System.out.println("Parent method1");
+    }
+    public void method2(){
+        System.out.println("Parent method2");
+    }
+}
